0756716ed6b8e835c9101bacc3d3fc9dc25a5af2
Gurshan <27gurshanhayer@usd266.com>
2024-03-28 12:45:43 -0500

    2nd assignment

--- Unit1/2InputAssignment.py
+++ Unit1/2InputAssignment.py
@@ -1,15 +1,41 @@
+#Exercise 1
 print("########## 1.2.1 ##########")
 
+name = input("What is your name?:")
+print(name)
+print(name)
+
+#Exercise 2
 print("########## 1.2.2 ##########")
 
+name = input("What is your name?:")
+print("!"+ name + "!" + name + "!")
+
+#Exercise 3
 print("########## 1.2.3 ##########")
 
+firstname = input("Enter your first name:")
+lastname = input("Enter your last name:")
+streetaddress = input("Enter your street address name:")
+cityaddress = input("Enter your address city:")
+stateaddress = input("Enter your address state:")
+zipcode = input("Enter your zip code:")
+print(firstname + " " + lastname)
+print(streetaddress)
+print(cityaddress + ", " + stateaddress + " " + zipcode)
+
+#Exercise 4
 print("########## 1.2.4 ##########")
 
 # Fix the code
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-print(part + part + part)
+part1 = input("The 1st part: ")
+part2 = input("The 2nd part: ")
+part3 = input("The 3rd part: ")
+print(part1 + "-" + part2 + "-" +  part3 + "!")
 
+#Exercise 5
 print("########## 1.2.5 ##########")
+
+name = input("Please type in a name:")
+year = input("Please type in a year:")
+print(name + " is a valiant knight, born in the year " + year + ". One morning " + name + " woke up to an awful racket: a dragon was approaching the village. Only " + name + " could save the village's residents.")
