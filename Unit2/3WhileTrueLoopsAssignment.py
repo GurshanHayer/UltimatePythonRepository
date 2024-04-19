0c758696c9e47bd745ed3da12e8131a19c873b70
Gurshan <27gurshanhayer@usd266.com>
2024-04-19 11:28:31 -0500

    completed past assignments

--- Unit2/3WhileTrueLoopsAssignment.py
+++ Unit2/3WhileTrueLoopsAssignment.py
@@ -1,85 +1,85 @@
-# from math import sqrt
-# # ========== 2.3.1 ==========
-# while True: 
-#     print("hi")
-#     what = input("Shall we continue? ")
-#     if what == "no":
-#         print("okay then")
-#         break
+from math import sqrt
+# ========== 2.3.1 ==========
+while True: 
+    print("hi")
+    what = input("Shall we continue? ")
+    if what == "no":
+        print("okay then")
+        break
 
-# # ========== 2.3.2 ==========
-# while True:
-#     n = int(input("Please type in a number: "))
-#     if n > 0:
-#         print(sqrt(n))
-#     elif n < 0:
-#         print("Invalid number")
-#     else:
-#         print("Exiting...")
-#         break
+# ========== 2.3.2 ==========
+while True:
+    n = int(input("Please type in a number: "))
+    if n > 0:
+        print(sqrt(n))
+    elif n < 0:
+        print("Invalid number")
+    else:
+        print("Exiting...")
+        break
 
-# # ========== 2.3.3 ==========
-# number = 5
-# print("Countdown!")
-# while True:
-#   print(number)
-#   number = number - 1
-#   if number <= 0:
-#     break
-# print("Now!")
+# ========== 2.3.3 ==========
+number = 5
+print("Countdown!")
+while True:
+  print(number)
+  number = number - 1
+  if number <= 0:
+    break
+print("Now!")
 
-# #========== 2.3.4 ==========
-# pas = input("Password: ")
-# while True:
-#     r = input("Repeat password: ")
-#     if r == pas:
-#         print("User account created!")
-#         break
-#     else:
-#         print("They do not match!")
+#========== 2.3.4 ==========
+pas = input("Password: ")
+while True:
+    r = input("Repeat password: ")
+    if r == pas:
+        print("User account created!")
+        break
+    else:
+        print("They do not match!")
 
-# # ========== 2.3.5 ==========
-# pin = 4321
-# attempts = 0
-# while True:
-#     p = int(input("Pin: "))
-#     attempts = attempts + 1
-#     if p == pin and attempts == 1:
-#         print("Correct! It only took you a single attempt!")
-#         break
-#     elif p == pin:
-#         print("Correct! It took you", attempts, "attempts")
-#         break
-#     else:
-#         print("Wrong")
+# ========== 2.3.5 ==========
+pin = 4321
+attempts = 0
+while True:
+    p = int(input("Pin: "))
+    attempts = attempts + 1
+    if p == pin and attempts == 1:
+        print("Correct! It only took you a single attempt!")
+        break
+    elif p == pin:
+        print("Correct! It took you", attempts, "attempts")
+        break
+    else:
+        print("Wrong")
 
-# # ========== 2.3.6 ==========
-# year = int(input("Year: "))
-# print("The next leap year after", year, "is", ((year - (year%4))+ 4))
+# ========== 2.3.6 ==========
+year = int(input("Year: "))
+print("The next leap year after", year, "is", ((year - (year%4))+ 4))
 
-# # ========== 2.3.7 ==========
-# string = ""
-# while True:
-#     word = input("Please type in a word: ")
-#     if word != "end":
-#         string = str(str(string) + str(word) + " ")
-#     else:
-#         print(str(string))
-#         break
-# # ========== 2.3.8 ==========
-# string = ""
-# pword = ""
-# word = ""
-# while True:
-#     pword = word
-#     word = input("Please type in a word: ")
-#     if word != "end" and word != pword:
-#         string = str(str(string) + str(word) + " ")
-#     else:
-#         print(str(string))
-#         break
+# ========== 2.3.7 ==========
+string = ""
+while True:
+    word = input("Please type in a word: ")
+    if word != "end":
+        string = str(str(string) + str(word) + " ")
+    else:
+        print(str(string))
+        break
+# ========== 2.3.8 ==========
+string = ""
+pword = ""
+word = ""
+while True:
+    pword = word
+    word = input("Please type in a word: ")
+    if word != "end" and word != pword:
+        string = str(str(string) + str(word) + " ")
+    else:
+        print(str(string))
+        break
 
-# # ========== 2.3.9 ==========
+# ========== 2.3.9 ==========
 n = ""
 sum = 0
 mean = 0
--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,21 +1,48 @@
-# ========== 3.1.1 ==========
+# # ========== 3.1.1 ==========
+# number = 30
+# while number >= 2:
+#     print(number)
+#     number = number - 2
 
-
-# ========== 3.1.2 ==========
-
+# # ========== 3.1.2 ==========
 # print("Are you ready?")
 # number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
+# while number != 0:
+#     print(number)
+#     number = number - 1
 # print("Go!")
 
-# ========== 3.1.3 ==========
+# # ========== 3.1.3 ==========
+# lim = int(input("Upper limit: "))
+# number = 1
+# while lim > number:
+#     print(number)
+#     number = number + 1
 
+# # ========== 3.1.4 ==========
+# lim = int(input("Upper limit: "))
+# number = 0
+# while lim > (2**number):
+#     print(2**number)
+#     number = number + 1
 
-# ========== 3.1.4 ==========
+# # ========== 3.1.5 ==========
+# lim = int(input("Upper limit: "))
+# base = int(input("Base: "))
+# number = 0
+# while lim >= (base**number):
+#     print(base**number)
+#     number = number + 1
 
+# # ========== 3.1.6 ==========
+# lim = int(input("Limit: "))
+# number = 1
+# total = 0
+# while True:
+#     if total < lim:
+#         total = total + number
+#         number = number + 1
+#     else:
+#         print(total)
+#         break
 
-# ========== 3.1.5 ==========
-
-
-# ========== 3.1.6 ==========
